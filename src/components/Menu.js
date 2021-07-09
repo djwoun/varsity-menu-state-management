7538abbb3fac8b4d52548e9cc436357ea401ac9b
djwoun <65102751+djwoun@users.noreply.github.com>
2021-07-09 23:37:26 +0900

    Revert "done"
    This reverts commit e6534be8b8d54bf7cd0d54f61557f038a0d8c2e2.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,75 +1,63 @@
-import React from "react";
-import MenuItem from "./MenuItem";
-import Receipt from "./Receipt";
+import React from 'react'
+import MenuItem from './MenuItem'
+import Receipt from './Receipt'
 
 const menuItems = [
   {
-    name: "2 Chili Dogs",
-    price: "$6.99",
+    name: '2 Chili Dogs',
+    price: '$6.99',
     picture:
-      "https://res.cloudinary.com/bog/image/upload/v1568150191/chili-dogs.jpg",
+      'https://res.cloudinary.com/bog/image/upload/v1568150191/chili-dogs.jpg',
   },
   {
-    name: "Classic Hamburger",
-    price: "$5.49",
+    name: 'Classic Hamburger',
+    price: '$5.49',
     picture:
-      "https://res.cloudinary.com/bog/image/upload/v1568150440/hamburger.jpg",
+      'https://res.cloudinary.com/bog/image/upload/v1568150440/hamburger.jpg',
   },
   {
-    name: "Glorified Bacon Cheeseburger",
-    price: "$6.99",
+    name: 'Glorified Bacon Cheeseburger',
+    price: '$6.99',
     picture:
-      "https://res.cloudinary.com/bog/image/upload/v1568150499/bacon-cheese.jpg",
+      'https://res.cloudinary.com/bog/image/upload/v1568150499/bacon-cheese.jpg',
   },
   {
-    name: "Chicken sandwich",
-    price: "$7.12",
+    name: 'Chicken sandwich',
+    price: '$7.12',
     picture:
-      "https://res.cloudinary.com/bog/image/upload/v1568150467/chicken-sammy.jpg",
+      'https://res.cloudinary.com/bog/image/upload/v1568150467/chicken-sammy.jpg',
   },
   {
-    name: "Classic Dog",
-    price: "$4.49",
+    name: 'Classic Dog',
+    price: '$4.49',
     picture:
-      "https://res.cloudinary.com/bog/image/upload/v1568150520/classic-dog.jpg",
+      'https://res.cloudinary.com/bog/image/upload/v1568150520/classic-dog.jpg',
   },
   {
-    name: "Classic cheeseburger",
-    price: "$5.99",
+    name: 'Classic cheeseburger',
+    price: '$5.99',
     picture:
-      "https://res.cloudinary.com/bog/image/upload/v1568150542/classic-cheese.jpg",
+      'https://res.cloudinary.com/bog/image/upload/v1568150542/classic-cheese.jpg',
   },
-];
+]
 
 function Menu() {
-  const [purchasedItems, setPurchasedItems] = React.useState([]);
-
-  const addPurchaseItem = purchasedItem => {
-    setPurchasedItems([...purchasedItems, purchasedItem]);
-  };
-
-  const removePurchasedItem = name => {
-    setPurchasedItems(purchasedItems.filter(item => name !== menuItems.name))
-  }
-
   return (
     <section>
       <dl>
-        {menuItems.map((menuItem) => {
+        {menuItems.map(menuItem => {
           return (
             <MenuItem
               name={menuItem.name}
               price={menuItem.price}
               picture={menuItem.picture}
-              addPurchaseItem={addPurchaseItem}
-              removePurchasedItem={removePurchasedItem}
             />
-          );
+          )
         })}
       </dl>
-      <Receipt purchasedItems={purchasedItems} />
+      <Receipt purchasedItems={[]} />
     </section>
-  );
+  )
 }
 
-export default Menu;
+export default Menu
