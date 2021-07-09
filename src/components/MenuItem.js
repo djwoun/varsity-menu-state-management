e6534be8b8d54bf7cd0d54f61557f038a0d8c2e2
djwoun <65102751+djwoun@users.noreply.github.com>
2021-07-09 23:37:16 +0900

    done

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const MenuItem = props => (
-  <button class="menu-item">
-    <img src={props.picture} alt={props.name} />
-    <dt>{props.name}</dt>
-    <dd>{props.price}</dd>
-  </button>
-)
-export default MenuItem
+const MenuItem = (props) => {
+  const [choose, setChoose] = React.useState(false);
+
+  const clickChoose = () => {
+    if (!choose) {
+      props.addPurchaseItem({
+        name: props.name,
+        price: props.price,
+      });
+    } else {
+      props.removePurchasedItem(props.name);
+    }
+    setChoose(!choose);
+  };
+
+  return (
+    <button
+      class={choose ? "menu-item selected" : "menu-item"}
+      onClick={clickChoose}
+    >
+      <img src={props.picture} alt={props.name} />
+      <dt>{props.name}</dt>
+      <dd>{props.price}</dd>
+    </button>
+  );
+};
+export default MenuItem;
